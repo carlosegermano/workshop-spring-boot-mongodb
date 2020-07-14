b2c938f9d5a92e576ddb30d054306036947a40d3
Carlos Eduardo <carlos.germano@dce.ufpb.br>
2020-07-14 20:31:08 -0300

    Instanciando a base de dados

---
+++ src/main/java/com/carlosegermano/workshopmongo/config/Instantiation.java
@@ -0,0 +1,30 @@
+package com.carlosegermano.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.carlosegermano.workshopmongo.domain.User;
+import com.carlosegermano.workshopmongo.repositories.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.deleteAll();
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}
