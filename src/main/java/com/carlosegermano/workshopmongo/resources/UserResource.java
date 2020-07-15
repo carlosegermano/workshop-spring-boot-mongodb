dd4f21bd26105b0b3c31b63b2fda6a3b92afcc35
Carlos Eduardo <carlos.germano@dce.ufpb.br>
2020-07-15 14:36:56 -0300

    Implementando busca de posts de usuario

--- src/main/java/com/carlosegermano/workshopmongo/resources/UserResource.java
+++ src/main/java/com/carlosegermano/workshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.carlosegermano.workshopmongo.domain.Post;
 import com.carlosegermano.workshopmongo.domain.User;
 import com.carlosegermano.workshopmongo.dtos.UserDTO;
 import com.carlosegermano.workshopmongo.services.UserService;
@@ -59,4 +60,9 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = userService.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
 }
